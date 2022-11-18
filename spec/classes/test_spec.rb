5b882670de34d168ac023e21e3398afb1abfee96
Helen Griffiths <threepistons@users.noreply.github.com>
2022-11-18 15:21:27 +0000

    PEBCAK: the 'let' must be inside a 'context'

--- spec/classes/test_spec.rb
+++ spec/classes/test_spec.rb
@@ -3,13 +3,13 @@
 require 'spec_helper'
 
 describe 'test' do
-
   on_supported_os.each do |os, os_facts|
-    let(:facts) { os_facts }
+    context "All tests so I don't have to repeat the 'let' line" do
+      let(:facts) { os_facts }
 
-    context "\"#{os}\" should match the failure message" do
-      it { is_expected.to compile.with_all_deps }
-      it { is_expected.to contain_notify('osfam').with('message' => %r{deadbeef}) }
+      context "\"#{os}\" should match the failure message" do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_notify('osfam').with('message' => %r{deadbeef}) }
     end
   end
 end
